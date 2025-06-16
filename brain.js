5f1396b13b312d0218ff68e4efb230b3f08f1d0e
VikaSh221525 <vikassharma221525@gmail.com>
2025-06-17 04:14:09 +0530

    pomodoro timer

--- brain.js
+++ brain.js
@@ -113,3 +113,92 @@ function motivationalquotes() {
     getquote();
 }
 motivationalquotes();
+
+
+
+function pomodorotimer() {
+    let totalsec = 1500;
+    let timerinterval = null;
+    let flag = false;
+    let timer = document.querySelector(".pomo-timer h5");
+    let start = document.querySelector(".pomo-timer .start-timer");
+    let pause = document.querySelector(".pomo-timer .pause-timer");
+    let reset = document.querySelector(".pomo-timer .reset-timer");
+    let session = document.querySelector("#session");
+    let isworksession = true;
+
+    function updatetime() {
+        let minutes = Math.floor(totalsec / 60);
+        let seconds = totalsec % 60;
+
+        timer.innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    }
+    updatetime();
+
+    function pausetimer() {
+        flag = false;
+        clearInterval(timerinterval);
+    }
+    function resettimer() {
+        flag = false;
+        totalsec = 1500;
+        clearInterval(timerinterval);
+        timerinterval = null;
+        updatetime();
+    }
+
+    function starttimer() {
+        if (flag == true) {
+            alert("please press pause");
+            return;
+        }
+        flag = true;
+        if (isworksession) {
+            timerinterval = setInterval(() => {
+                if (totalsec > 0) {
+                    totalsec--;
+                    updatetime();
+                }
+                else {
+                    isworksession = false;
+                    clearInterval(timerinterval);
+                    timerinterval = null;
+                    flag = false;
+                    timer.innerHTML = "05:00";
+                    session.innerHTML = "Break session";
+                    session.style.backgroundColor = "#00a8ff";
+                    totalsec = 300;
+                    // updatetime();
+                }
+            }, 1000)
+        }
+        else {
+
+            session.innerHTML = "Break session";
+            session.style.backgroundColor = "#00a8ff";
+            timerinterval = setInterval(() => {
+                if (totalsec > 0) {
+                    totalsec--;
+                    updatetime();
+                }
+                else {
+                    isworksession = true;
+                    clearInterval(timerinterval);
+                    timerinterval = null;
+                    flag = false;
+                    timer.innerHTML = "25:00";
+                    session.innerHTML = "Work session";
+                    session.style.backgroundColor = "#098A54";
+                    totalsec = 1500;
+                    // updatetime();
+                }
+            }, 1000)
+        }
+    }
+
+
+    start.addEventListener("click", () => flag ? alert("please press pause") : starttimer());
+    pause.addEventListener("click", pausetimer);
+    reset.addEventListener("click", resettimer);
+}
+pomodorotimer();
